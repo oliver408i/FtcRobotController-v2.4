d1193e591cb95061c37ac70df9ad1c63290bd58c
rianbutala <rianbutala@gmail.com>
2023-09-28 16:40:31 -0700

    added old drive files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDynamicPlatform.java
@@ -0,0 +1,143 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Basic program that does teleop. This is supposed to be a function-based,
+ * modular program.
+ */
+
+@TeleOp(name = "Omni Op Test Platform", group= "Linear Opmode")
+public class TeleOpDynamicPlatform extends LinearOpMode {
+    RobotHardware robot = new RobotHardware();
+    private ElapsedTime runtime = new ElapsedTime();
+    double additionalYaw = 0;
+    double leftYawCoolDown = runtime.seconds();
+    double rightYawCoolDown = runtime.seconds();
+
+    boolean isHandedOff = false;
+    //TODO: handoff if plate wants it
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // initalizes
+
+        waitForStart();
+        robot.init(hardwareMap);
+        runtime.reset();
+        //starts after initialization (press start)
+
+        // run until stop is pressed
+        while (opModeIsActive()) {
+            double max; // get top wheel speed
+
+            double regularSpeed = 0.5;
+            double superSpeed = 1;
+            //superspeed is used only when there's a stretch
+            // of ground to be covered, normally not
+            // to be used during matches
+
+            double axial   = -gamepad1.left_stick_y * regularSpeed;  // Note: pushing stick forward gives negative value
+            double lateral =  gamepad1.left_stick_x * regularSpeed;
+            double yaw     =  gamepad1.right_stick_x* regularSpeed;
+
+            if(gamepad1.left_bumper){
+                axial   *=  superSpeed/regularSpeed; //recalculating all values, least year's method was less elegant
+                lateral *=  superSpeed/regularSpeed;
+                yaw     *=  superSpeed/regularSpeed;
+            }
+
+            double leftFrontPower  = (axial + lateral + yaw);
+            double rightFrontPower = (axial - lateral - yaw);
+            double leftBackPower   = (axial - lateral + yaw);
+            double rightBackPower  = (axial + lateral - yaw);
+            // movement algorithm
+
+            double right_trig = gamepad1.right_trigger;
+            double left_trig  = gamepad1.left_trigger;
+            // for strafing
+
+            double avgMotorPower = (leftBackPower+leftFrontPower+rightBackPower+rightFrontPower)/4;
+            // additionalyaw resource. we need to turn proportional to average speed of bot, and while this isn't perfect it works
+
+
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+            // calculate top wheel speed
+
+            if (gamepad1.dpad_left && (runtime.seconds()-leftYawCoolDown)>1){
+                additionalYaw-=0.01;
+                leftYawCoolDown = runtime.seconds();
+            }
+            // need a cooldown for additionalyaw or it'll keep adding
+
+            if (gamepad1.dpad_right && (runtime.seconds()-rightYawCoolDown)>1){
+                additionalYaw+=0.01;
+                rightYawCoolDown = runtime.seconds();
+            }
+/*
+            if(gamepad2.dpad_up){
+                robot.singleMotorEncoderMovements(telemetry,2,0.05,robot.EncoderTest);
+            }
+
+ */
+
+            if(gamepad1.right_trigger > 0){
+                leftFrontPower = right_trig;
+                leftBackPower = -right_trig;
+                rightFrontPower = -right_trig;
+                rightBackPower = right_trig;
+            }
+
+            if(gamepad1.left_trigger > 0){
+                leftFrontPower = -left_trig;
+                leftBackPower = left_trig;
+                rightFrontPower = left_trig;
+                rightBackPower = -left_trig;
+            }
+
+            telemetry.addData("vector to degree test: ",robot.vectorToDegrees(axial,lateral));
+            // i believe this is calculating angle of the robot relative to starting point
+
+            if (max > 1) {
+                leftFrontPower  /= max;
+                rightFrontPower /= max;
+                leftBackPower   /= max;
+                rightBackPower  /= max;
+            }
+
+            leftBackPower += additionalYaw*avgMotorPower;
+            leftFrontPower += additionalYaw*avgMotorPower;
+            rightBackPower -= additionalYaw*avgMotorPower;
+            rightFrontPower -= additionalYaw*avgMotorPower;
+
+            robot.leftFront.setPower(leftFrontPower*superSpeed);
+            robot.rightFront.setPower(rightFrontPower*superSpeed);
+            robot.leftBack.setPower(leftBackPower*superSpeed);
+            robot.rightBack.setPower(rightBackPower*superSpeed);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.addData("Left Trigger:" , gamepad1.left_trigger);
+            telemetry.addData("Right Trigger", gamepad1.right_trigger);
+            telemetry.addData("Left Stick Y", gamepad1.left_stick_y);
+            telemetry.addData("Left Stick X", gamepad1.left_stick_x);
+            telemetry.addData("Right Stick Y", gamepad1.right_stick_y);
+            telemetry.addData("Right Stick X", gamepad1.right_stick_x);
+            telemetry.addData("Additional Yaw: ", additionalYaw);
+            telemetry.addData("Average Motor Power: ", avgMotorPower);
+            telemetry.addData("Axial: ", axial);
+            telemetry.addData("Lateral: ", lateral);
+            telemetry.addData("Yaw: ",yaw);
+            telemetry.update();
+        }
+    }
+}
