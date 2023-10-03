4b691faf77bde227c2d08f808efafe029b2b4670
Rufan <rufanyin29a@gmail.com>
2023-10-02 17:34:27 -0700

    Tuning X

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -36,7 +36,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
 
     // According to roadrunner docs this can have 1% difference but I'm not bothering so yea
     // https://learnroadrunner.com/dead-wheels.html#adjusting-the-wheel-radius-2
-    public static double X_MULTIPLIER = 1; // Multiplier in the X direction
+    public static double X_MULTIPLIER = 1.043478260869565; // Multiplier in the X direction
     public static double Y_MULTIPLIER = 1; // Multiplier in the Y direction
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
