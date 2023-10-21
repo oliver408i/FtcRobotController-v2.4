0e0c85dfc3e7c521691a8b59958f84d8b020c819
rianbutala <rianbutala@gmail.com>
2023-10-21 14:43:45 -0700

    custom spline made. lots to learn in the future.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomSplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomSplineTest.java
@@ -2,21 +2,27 @@ package org.firstinspires.ftc.teamcode;
 
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
+@Autonomous(name = "Custom Spline Test")
 public class CustomSplineTest extends LinearOpMode{
-    RobotHardware robot = new RobotHardware();
+    //RobotHardware robot = new RobotHardware();
 
     @Override
     public void runOpMode() {
-        robot.init(hardwareMap);
+        //robot.init(hardwareMap);
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
-                .splineToSplineHeading(new Pose2d(10,20,0), 180)
+                .splineToConstantHeading(new Vector2d(10,20), Math.toRadians(0))
+                /*.splineToConstantHeading(new Vector2d(-10,20), Math.toRadians(0))
+                .splineToConstantHeading(new Vector2d(10,-40), Math.toRadians(0))*/
                 .build();
 
         waitForStart();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -88,7 +88,7 @@ public class DriveConstants {
 
      */
     public static double MAX_VEL = 52.48291908330528;
-    public static double MAX_ACCEL = 52.48291908330528/2; //52.48291908330528
+    public static double MAX_ACCEL = 52.4829190833052/2; //52.48291908330528
     public static double MAX_ANG_VEL = Math.toRadians(194.00321032258066); //194.00321032258066 or 182.3177943230887
     public static double MAX_ANG_ACCEL = Math.toRadians(194.00321032258066);
 
