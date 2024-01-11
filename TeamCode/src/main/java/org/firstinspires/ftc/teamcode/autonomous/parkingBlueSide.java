90b74b8e6b67e63e420bef54320d47dda5779ae8
rianbutala <rianbutala@gmail.com>
2024-01-11 11:51:51 -0800

    increase intake power and parkingBlueSide opmode name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/parkingBlueSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/parkingBlueSide.java
@@ -59,7 +59,7 @@ import java.util.concurrent.TimeUnit;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@Autonomous(name = "parkingRedSide", group = "Autonomous")
+@Autonomous(name = "parkingBlueSide", group = "Autonomous")
 
 public class parkingBlueSide extends LinearOpMode {
 
